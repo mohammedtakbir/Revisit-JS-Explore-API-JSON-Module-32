508d218d29c70e3390217557ab08ff86ca2b6313
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-25 13:52:04 +0600

    explore JSON, JSON.stringify() and JSON.parse()

---
+++ json.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Robin',
+    address: 'Narayanganj',
+    isMarried: false,
+    income: 8000,
+    products: ['button mobile']
+};
+console.log(person)
+console.log(JSON.stringify(person))
